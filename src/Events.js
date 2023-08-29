8ad95d457bf493802ed8de573f2aaa91503f11ab
Becca <rebecca_waterson@brown.edu>
2023-08-29 01:52:18 -0400

    iframe

--- src/Events.js
+++ src/Events.js
@@ -7,8 +7,7 @@ export default function Events() {
       <div className="calendar">
       <iframe 
       className='calendarFrame'
-      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%233F51B5&ctz=America%2FNew_York&showTitle=0&src=Y18yOGQyZDA1MjMxYWRmYzQ1MDRhYjA3MWRkMTVlMjRlODZlZWIyM2ZmNDBkNGNmZDRmOGFmMWFkNjcyYWZiMWEwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=Z2w2cjg0aTlwbWdoaTE0c2FtaTI5bmxrdDBraWZuOWRAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%233F51B5&color=%23A79B8E&color=%23AD1457"        
-      ></iframe>
+      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23B39DDB&ctz=America%2FNew_York&src=Y18yOGQyZDA1MjMxYWRmYzQ1MDRhYjA3MWRkMTVlMjRlODZlZWIyM2ZmNDBkNGNmZDRmOGFmMWFkNjcyYWZiMWEwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%233F51B5"       ></iframe>
       </div>
       
     </div>
