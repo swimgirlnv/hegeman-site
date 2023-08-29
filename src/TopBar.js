36f25a751392d8ad7d42f78384e4a8adcf398ed6
Becca <rebecca_waterson@brown.edu>
2023-08-29 01:39:35 -0400

    fixing nav bar

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,17 +1,42 @@
 import './App.css'
+import { FaBars } from "react-icons/fa";
+
 
 function TopBar () {
   return (
-    <div className="top-bar" id="navbar">
+    <div className="topNavigation" id="navbar">
           <a className="navbar-first" href="#Intro"><strong>Welcome!</strong></a>
-          <a href="#About">Your CCs</a>
-          <div className="separator"></div>
-          <a className="navbar-last" href="#Links">Important Links</a>
-          <div className="separator"></div>
-          <a className="navbar-last" href="#Events">Upcoming Events</a>
-          <div className="separator"></div>
-          <a className="navbar-last" href="#FAQs">FAQs</a>
+          <div className='navLinks'>
+            <a href="#About">Your CCs</a>
+            <div className="separator"></div>
+            <a className="navbar-last" href="#Links">Important Links</a>
+            <div className="separator"></div>
+            <a className="navbar-last" href="#Events">Upcoming Events</a>
+            <div className="separator"></div>
+            <a className="navbar-last" href="#FAQs">FAQs</a>
+          </div>
+          
+
+          <div className='navLinksMini' id="navLinksMini">
+            <a href="#About">Your CCs</a>
+            <a className="navbar-last" href="#Links">Important Links</a>
+            <a className="navbar-last" href="#Events">Upcoming Events</a>
+            <a className="navbar-last" href="#FAQs">FAQs</a>
+          </div>
+          <div
+          className='hamburgerIcon'
+          onClick={() => {
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            const sidebar = document.querySelector(`.navLinksMini`);
+            if (sidebar) {
+              // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+              sidebar.classList.toggle(`open`);
+            }
+          }}
+        >
+          <FaBars className='hamburger' />
         </div>
+    </div>
   );
 }
 
