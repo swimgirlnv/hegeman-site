37c10f44ac522b1c5cd67a55a2c76456f8d7975a
Becca <rebecca_waterson@brown.edu>
2023-09-01 07:42:23 -0400

    adding in first event!

--- src/Events.js
+++ src/Events.js
@@ -9,6 +9,11 @@ export default function Events() {
       className='calendarFrame'
       src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23B39DDB&ctz=America%2FNew_York&src=Y18yOGQyZDA1MjMxYWRmYzQ1MDRhYjA3MWRkMTVlMjRlODZlZWIyM2ZmNDBkNGNmZDRmOGFmMWFkNjcyYWZiMWEwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%233F51B5"       ></iframe>
       </div>
+      <div className='posters'>
+        <div className='poster'>
+          <img className='posterImg' src='https://i.imgur.com/SjH4ge5.png' />
+        </div>
+      </div>
       
     </div>
 
--- src/Forms.js
+++ src/Forms.js
@@ -3,15 +3,16 @@ import './forms.css'
 export default function Forms() {
 
   return (
-    <div className="item3" id="Events">
+    <div className="item3" id="FAQs">
       <div className="calendar">
       <iframe 
       className='formFrame'
-      src="https://docs.google.com/forms/d/e/1FAIpQLSf9MfnKJOL-dgwXpj-4RKOUBLaLZbOcwB3U1jxq9ENajgdfkA/viewform?embedded=true"
-      ></iframe>
+      src="https://docs.google.com/forms/d/e/1FAIpQLSflOPNpVbCl9UReEC-zdKN74Z9m639gSJmuZKkN7-REUnTYcQ/viewform?embedded=true"      ></iframe>
       </div>
       
     </div>
 
   );
 }
+
+
